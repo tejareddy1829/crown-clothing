c051d3086c6ba31afb41d9f18a930f75650c459a
tejareddy1829 <tejar9700565670@gmail.com>
2022-11-22 22:22:47 +0530

    context

--- src/components/sign-up-form/sign-up-form.components.jsx
+++ src/components/sign-up-form/sign-up-form.components.jsx
@@ -1,10 +1,12 @@
-import {useState} from "react";
+import {useState, useContext} from "react";
 
 import FormInput from "../form-input/form-input.components";
 
 import Button from "../button/button.component";
 
 import "./sign-up-form.styles.scss";
+
+import { UserContext } from "../../contexts/user.context";
 
 import {
   createAuthUserWithEmailAndPassword,
