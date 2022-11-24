0a68259629d2d33f601a58da0833b4efe292c6df
tejareddy1829 <tejar9700565670@gmail.com>
2022-11-24 14:19:18 +0530

    cart-dropdown

--- src/routes/navigation/navigation.components.jsx
+++ src/routes/navigation/navigation.components.jsx
@@ -1,17 +1,21 @@
 import {Fragment, useContext} from "react";
 
+import CartIcon from "../../components/cart-icon/cart-icon.components";
+import CartDropDown from "../../components/cart-dropdown/cart-dropdown.components";
+
 import {ReactComponent as Crownlogo} from "../../assets/crown.svg";
 
 import "./navigation.styles.scss";
 import {UserContext} from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import {Outlet, Link} from "react-router-dom";
 
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
   const {currentUser} = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
 
- 
   return (
     <Fragment>
       <div className='navigation'>
@@ -31,7 +35,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropDown />}
       </div>
       <Outlet />
     </Fragment>
--- src/routes/shop/shop.components.jsx
+++ src/routes/shop/shop.components.jsx
@@ -3,10 +3,12 @@ import {useContext} from "react";
 import {ProductsContext} from "../../contexts/products.context";
 import ProductCard from "../../components/product-card/product-card.components";
 
+import './shop.styles.scss'
+
 const Shop = () => {
   const {products} = useContext(ProductsContext);
   return (
-    <div>
+    <div className='products-container'>
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
