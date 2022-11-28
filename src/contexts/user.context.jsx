7cb662c5920da1fee139e5aa1c2f6122e717e6bc
tejareddy1829 <tejar9700565670@gmail.com>
2022-11-28 15:44:01 +0530

    reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,18 +1,54 @@
-import { createContext, useState, useEffect } from 'react';
+import {createContext, useEffect, useReducer} from "react";
+
+import {createAction} from "../utils/reducer/reducer.utils";
 
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-} from '../utils/firebase/firebase.utils';
+} from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
   currentUser: null,
 });
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const {type, payload} = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`unhandled typr ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+export const UserProvider = ({children}) => {
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch(
+      createAction(
+        USER_ACTION_TYPES.SET_CURRENT_USER,
+        USER_ACTION_TYPES.SET_CURRENT_USER
+      )
+    );
+  };
+
+  const value = {currentUser, setCurrentUser};
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
