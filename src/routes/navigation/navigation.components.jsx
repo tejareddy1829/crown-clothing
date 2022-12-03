6383cc8e048bca250e19de1e1fd553fd37a22ccd
tejareddy1829 <tejar9700565670@gmail.com>
2022-12-03 14:09:47 +0530

    user-saga

--- src/routes/navigation/navigation.components.jsx
+++ src/routes/navigation/navigation.components.jsx
@@ -1,6 +1,6 @@
 import {Fragment} from "react";
 import {Outlet} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 
 import CartIcon from "../../components/cart-icon/cart-icon.components";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.components";
@@ -8,7 +8,6 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 import {selectCurrentUser} from "../../store/user/user.selector";
 import {selectIsCartOpen} from "../../store/cart/cart.selector";
 import {ReactComponent as CrwnLogo} from "../../assets/crown.svg";
-import {signOutUser} from "../../utils/firebase/firebase.utils";
 
 import {
   NavigationContainer,
@@ -16,12 +15,15 @@ import {
   NavLink,
   LogoContainer,
 } from "./navigation.styles";
+import {signOutStart} from "../../store/user/user.action";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
 
-  
+  const signOutUser = () => dispatch(signOutStart());
+
   return (
     <Fragment>
       <NavigationContainer>
